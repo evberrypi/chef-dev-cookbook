a9483f3ee65012a086d57390dcd935fbbfb6ae27
vagrant <vagrant@vagrant.vm>
2018-11-16 00:47:37 +0000

    adding nodejs

--- cookbooks/dev/recipes/default.rb
+++ cookbooks/dev/recipes/default.rb
@@ -4,9 +4,9 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
-execute 'install nvm' do
-	command 'curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash'
-	command 'source ~/.bashrc'
-	command 'nvm install v11'
+execute 'npm_install' do
+  command 'npm install'
+  user 'nodejs'
+  cwd '/opt/nodejs/myapp/'
+  action :nothing
 end
-
